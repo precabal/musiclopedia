291e261fb3bd33f03f8fe3ba69e0ef45a75861cb
Ubuntu <ubuntu@ip-172-31-27-55.ec2.internal>
2015-09-16 23:37:57 +0000

    reorganized project structure and set it up as maven project

---
+++ sparkUtils/src/main/java/com/precabal/app/App.java
@@ -0,0 +1,13 @@
+package com.precabal.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
