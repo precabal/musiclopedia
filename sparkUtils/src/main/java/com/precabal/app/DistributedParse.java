c35f44b7d284046f9309348d7f7042fb80754e6d
precabal <pablorecabal@gmail.com>
2015-09-17 18:35:40 -0400

    trying out the linbreak supressor

--- sparkUtils/src/main/java/com/precabal/app/DistributedParse.java
+++ sparkUtils/src/main/java/com/precabal/app/DistributedParse.java
@@ -16,54 +16,43 @@ import java.util.regex.Pattern;
 
 public final class DistributedParse {
 	
-	private static final Pattern SPACE = Pattern.compile(" ");
+	private static final Pattern LINE_BREAK = Pattern.compile(System.lineSeparator());
 
 	public static void main(String[] args) throws Exception {
 
 		/* error checking */
-
     	if (args.length < 1) {
       		System.err.println("Usage: JavaWordCount <file>");
       		System.exit(1);
     	}
 		
+		/* setup TODO: mod this to include subsequent registers */    	
+    	
     	Configuration conf = new Configuration();
-    	//TODO: mod this to include following registers
     	conf.set("textinputformat.record.delimiter", "WARC/1.0");
-
- 
-		/* setup and read text file */
     	SparkConf sparkConf = new SparkConf().setAppName("JavaSearchForExpression");
 		JavaSparkContext context = new JavaSparkContext(sparkConf);		
-		JavaPairRDD<LongWritable,Text> lines = context.newAPIHadoopFile(args[0], TextInputFormat.class, LongWritable.class, Text.class, conf);
 		
-		/* process each line to remove the linebreak except for the headers. */
-		/*
-		JavaRDD<String> strippedLines = lines.map(new Function<String, String>() {
+		
+		/* read text file */
+		
+		JavaRDD<Text> lines = context.newAPIHadoopFile(args[0], TextInputFormat.class, LongWritable.class, Text.class, conf).values();
+		
+		
+		/* process each line to remove the linebreak */
+		
+		JavaRDD<String> linesNoBreaks = lines.map(new Function<Text, String>() {
+		
 			@Override
-			public String call(String s) {
-				
-				if ( s.equals("WARC/1.0") )
-					s = System.lineSeparator().concat(s);
-				
-				//see why this in not working
-				s.concat("poto");
-				return s;
+			public String call(Text input) {
+				input.toString().replace(System.lineSeparator(), " ");
+				return input.toString();
 			}
 		});
-		*/
 		
-		/* save output */
-		/*
-		List<String> linesNonRDD = strippedLines.collect();
 		
-		for(int i = 0; i<linesNonRDD.size(); i++)
-		{
-			//ADD A SPACE FOR EACH LINE BREAK
-			System.out.print(linesNonRDD.get(i));
-		}
-		*/
-
+		/* save output */		
+		
 		lines.saveAsTextFile("hdfs://ec2-54-210-182-168.compute-1.amazonaws.com:9000/user/outputText");
 	
 		context.stop();
