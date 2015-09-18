8a6ed3f4ef08a70f717a6d9eb377f6b5765cc833
precabal <pablorecabal@gmail.com>
2015-09-18 00:06:06 -0400

    import

--- sparkUtils/src/main/java/com/precabal/app/DistributedParse.java
+++ sparkUtils/src/main/java/com/precabal/app/DistributedParse.java
@@ -6,6 +6,7 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.PairFunction;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.LongWritable;
