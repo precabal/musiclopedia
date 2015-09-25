b936b793d8ef6fde059126cc7ac874551a03e99f
precabal <pablorecabal@gmail.com>
2015-09-25 02:40:00 -0400

    added scripts and working version of the DB Manager

--- GraphDB_Interface/src/DB_Manager.java
+++ GraphDB_Interface/src/DB_Manager.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.orientechnologies.orient.core.metadata.schema.OSchema;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.orient.OrientGraph;
 import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory;
@@ -14,36 +15,42 @@ public class DB_Manager {
 	
 	public static void main(String[] args) {
 
-    	if (args.length < 2) {
-      		System.err.println("Usage: DistributedParse <inputKeys> <edges>");
+    	if (args.length < 3) {
+      		System.err.println("Usage: DB_Manager <inputKeys> <edges> <db_path>");
       		System.exit(1);
     	}
     	String keysFilePath = args[0];
     	String edgesFilePath = args[1];
+    	String dbPath = args[2]; //"plocal:/home/ubuntu/project/releases/orientdb-community-2.1.2/databases/db"
     	
     	DB_Manager manager = new DB_Manager();
     	
-    	manager.loadVerticesAndEdges(keysFilePath, edgesFilePath);
+    	manager.loadVerticesAndEdges(keysFilePath, edgesFilePath, dbPath);
     	
 		
     	
 	}
 
-	public void loadVerticesAndEdges(String keys, String edges) {
+	public void loadVerticesAndEdges(String keys, String edges, String databasePath) {
 		
-		OrientGraphFactory factory = new OrientGraphFactory("plocal:~/data/temp/graph/db");
+		OrientGraphFactory factory = new OrientGraphFactory(databasePath);
 		graph = factory.getTx();
 		
-		graph.createVertexType("url");
-		graph.createVertexType("artist");
+		if(graph.getVertexType("url")==null)
+			graph.createVertexType("url");
+		if(graph.getVertexType("artist")==null)
+			graph.createVertexType("artist");
 		
 		try{
 			Map<String,Vertex> vertexMap = loadVertices(keys);
+			System.out.println("done vertex");
 			insertEdges(edges,vertexMap);
 			
 			graph.commit();
 			
 		} catch( Exception e ) {
+			System.out.println("caught an exeption");
+			e.printStackTrace();
 			graph.rollback();
 
 		} finally {
@@ -57,10 +64,12 @@ public class DB_Manager {
 		
 		Map<String,Vertex> vertexMap = new HashMap<String,Vertex>();
         
-		
+		System.out.println("im vertex");
 		BufferedReader inputReader = new BufferedReader(new FileReader(inputFile));	
 		String line = inputReader.readLine();
+		System.out.println(line);
 		while(line!=null){
+			
 			Vertex vertex = graph.addVertex("class:artist");
 			vertex.setProperty("name", line);
 			vertexMap.put(line, vertex);
@@ -79,11 +88,13 @@ public class DB_Manager {
 		String line = inputReader.readLine();
 		while(line!=null){
 			String url = line.split(",")[0];
+			
+			
 			Vertex urlVertex = graph.addVertex("class:url");
 			urlVertex.setProperty("name", url);
 			
 			String artist = line.split(",")[1];
-			
+			System.out.println(artist);
 			/* consider using graph.getVertexByKey("name", artist) isntead */
 			graph.addEdge(null,urlVertex, vertexMap.get(artist), "contains");
 			
---
+++ serialize.sh
@@ -0,0 +1,47 @@
+#/bin/bash
+HOST_DNS='hdfs://ec2-54-210-182-168.compute-1.amazonaws.com:9000/'
+ITERATIONS=$1
+DATA_FLDR='data'
+FILE_PATHS=$DATA_FLDR'/wet.paths'
+PREFIX='https://aws-publicdatasets.s3.amazonaws.com/'
+COUNTER=0
+OUTPUT_FOLDER_PREFIX=$HOST_DNS'user/data/output_'
+HDFS_FOLDER='/user/data/inputText/'
+
+hdfs dfs -rm -r /user/data/output_*
+while [  $COUNTER -lt $ITERATIONS ]; do
+     
+     let COUNTER=COUNTER+1 
+
+     #read n-th line of wet files paths
+     CURRENT_FILE=$(sed ''$COUNTER'q;d' $FILE_PATHS | cut --characters=71-)
+     CURRENT_FILE_PATH=$(sed ''$COUNTER'q;d' $FILE_PATHS)
+     echo '##### current file  = '$PREFIX$CURRENT_FILE
+     echo '##### output folder = '$OUTPUT_FOLDER_PREFIX$COUNTER
+	 #downloads the current file to a temp directory
+	 
+	 #echo '##### wget' $PREFIX$CURRENT_FILE_PATH
+	 wget $PREFIX$CURRENT_FILE_PATH -O $DATA_FLDR'/tmp/'$CURRENT_FILE
+	 gunzip -f $DATA_FLDR'/tmp/'$CURRENT_FILE 
+
+	 CURRENT_FILE_UNZIPPED=${CURRENT_FILE::-3}
+	 echo '##### this is '$CURRENT_FILE_UNZIPPED
+
+
+	 #moves to HDFS
+	 hdfs dfs -moveFromLocal $DATA_FLDR'/tmp/'$CURRENT_FILE_UNZIPPED $HDFS_FOLDER
+
+	 #calls the spark-submit with the recently downloaded file and the output folder name
+	 time spark-submit \
+	 --class sparkUtils.DistributedParse \
+	 --master spark://ip-172-31-27-55:7077 \
+	 --executor-memory 4G \
+	 bin/DistributedParse.jar \
+	 $HOST_DNS$HDFS_FOLDER$CURRENT_FILE_UNZIPPED \
+	 data/jazzMusicians.txt \
+	 $OUTPUT_FOLDER_PREFIX$COUNTER
+	 #3. delete the downloaded file form local and remote
+	 rm -rf $DATA_FLDR'/tmp/'$CURRENT_FILE_UNZIPPED
+	 hdfs dfs -rm /user/data/inputText/$CURRENT_FILE_UNZIPPED
+
+done
