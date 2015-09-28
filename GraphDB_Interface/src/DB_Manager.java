4165d7c88aaeb7a59a737a2b84c22ee4171f0089
precabal <pablorecabal@gmail.com>
2015-09-28 17:44:07 -0400

    refactored class to receive an HDFS directory to read files from

--- GraphDB_Interface/src/DB_Manager.java
+++ GraphDB_Interface/src/DB_Manager.java
@@ -18,6 +18,8 @@ import org.apache.hadoop.io.compress.CompressionCodec;
 import org.apache.hadoop.io.compress.CompressionCodecFactory;
 
 import com.orientechnologies.orient.core.metadata.schema.OSchema;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.sql.OCommandSQL;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.orient.OrientGraph;
 import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory;
@@ -25,6 +27,7 @@ import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory;
 public class DB_Manager {
 	
 	OrientGraph graph;
+	private Map<String,Vertex> vertexMap;
 	
 	public static void main(String[] args) {
 
@@ -55,8 +58,8 @@ public class DB_Manager {
 			graph.createVertexType("artist");
 		
 		try{
-			Map<String,Vertex> vertexMap = insertVertices(keys);
-			insertEdges(edges,vertexMap);
+			vertexMap = insertVertices(keys);
+			insertEdges(new Path(edges));
 			
 			graph.commit();
 			
@@ -95,85 +98,80 @@ public class DB_Manager {
 		
     }
 	
-    private void insertEdges(String inputDirectory, Map<String, Vertex> vertexMap) {
- 
-
-    	Map<String,Vertex> urlVertexMap = new HashMap<String,Vertex>();
+    private void insertEdges(Path inputDirectory) throws IllegalArgumentException, IOException{
     	
-		File[] files = new File(inputDirectory).listFiles();
-		
-	    for (File file : files) {
-	        if (!file.isDirectory()) {
-	        	String path = inputDirectory.concat("/").concat(file.getName());
-	    
-	        	Path location = new Path(path);
-	        	
-	        	List<String> results;
-				try {
-					results = readLines(location, new Configuration());
-
-					for(String line : results){
-
-						String url = line.split(",")[0];
-
-						Vertex urlVertex = urlVertexMap.get(url);
-						if(urlVertex==null){
-							urlVertex = graph.addVertex("class:url");
-							urlVertex.setProperty("name", url);
-							urlVertexMap.put(url, urlVertex);
-						}
-
-						String artist = line.split(",")[1];
-
-						Vertex artistVertex = vertexMap.get(artist);
-						if(artistVertex!=null)
-							graph.addEdge(null,urlVertex, artistVertex, "contains");
-
-
-					}
-				} catch (Exception e) {
-					System.out.println("Can't load file : " + path);
-					e.printStackTrace();
-				}
-	    		
-	        }
-	    }
-
+    	FileSystem fileSystem = FileSystem.get(inputDirectory.toUri(), new Configuration());
+    	/* get all files and folders */
+    	FileStatus[] items = fileSystem.listStatus(inputDirectory);
+    	
+    	for(FileStatus item : items){
+    		
+            // ignoring files like _SUCCESS
+            if(item.getPath().getName().startsWith("_")) {
+              continue;
+            }
+    		
+    		if(item.isDirectory()){
+    			/* if file is a folder, call itself recursively */
+    			insertEdges(item.getPath());
+    		}else{
+    	    	/* if it's a file, get the lines within it */
+    	    	List<String> lines = getLines("path", fileSystem);
+    			
+    	    	/*and add them as vertices to the db */
+    	    	for(String line : lines)
+    	    		addLineToDB(line);    	    	
+    		}
+    	}
 
     }
-    public List<String> readLines(Path location, Configuration conf) throws Exception {
-        FileSystem fileSystem = FileSystem.get(location.toUri(), conf);
-        CompressionCodecFactory factory = new CompressionCodecFactory(conf);
-        FileStatus[] items = fileSystem.listStatus(location);
-        if (items == null) return new ArrayList<String>();
-        List<String> results = new ArrayList<String>();
-        for(FileStatus item: items) {
-
-          // ignoring files like _SUCCESS
-          if(item.getPath().getName().startsWith("_")) {
-            continue;
-          }
-
-          CompressionCodec codec = factory.getCodec(item.getPath());
-          InputStream stream = null;
-
-          // check if we have a compression codec we need to use
-          if (codec != null) {
-            stream = codec.createInputStream(fileSystem.open(item.getPath()));
-          }
-          else {
-            stream = fileSystem.open(item.getPath());
-          }
-
-          StringWriter writer = new StringWriter();
-          IOUtils.copy(stream, writer, "UTF-8");
-          String raw = writer.toString();
-          String[] resulting = raw.split("\n");
-          for(String str: raw.split("\n")) {
+    private List<String> getLines(String HDFSinputFilePath, FileSystem referenceFileSystem) throws IllegalArgumentException, IOException {
+    	   
+    	List<String> results = new ArrayList<String>();
+    	
+    	InputStream stream  = null;
+    	
+		stream = referenceFileSystem.open(new Path(HDFSinputFilePath));
+		
+    	StringWriter writer = new StringWriter();
+        
+		IOUtils.copy(stream, writer, "UTF-8");
+		
+        String raw = writer.toString();
+        
+        for(String str: raw.split("\n")) {
             results.add(str);
-          }
         }
-        return results;
-      }
+    	
+    	return results;
+    }
+    
+    private void addLineToDB(String line){
+
+    	String url = line.split(",")[0];
+    	String artist = line.split(",")[1];
+    	
+    	Vertex urlVertex = null;
+    	
+    	String query = "SELECT * FROM  " + "url" + " WHERE " + "name" + " = ?";
+    	List<ODocument> qResult = graph.command(new OCommandSQL(query)).execute(url);
+    	
+    	if (qResult.isEmpty()){
+    		urlVertex = graph.addVertex("class:url");
+    		urlVertex.setProperty("name", url);
+    	}else{
+    		urlVertex = graph.getVertex(qResult);
+    	}
+
+    	Vertex artistVertex = vertexMap.get(artist);
+    	if(artistVertex!=null)
+    		graph.addEdge(null,urlVertex, artistVertex, "contains");
+    	else{
+    		System.out.println("could not find artist "+artist);
+    	}
+
+    }
+    
+
  
 }
