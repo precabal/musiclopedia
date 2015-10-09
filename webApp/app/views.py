e75a2d35fb93c6b6711db0d70d7ffb366b3f0aed
precabal <pablorecabal@gmail.com>
2015-10-08 20:51:38 -0400

    added concurrent databases reference

--- webApp/app/views.py
+++ webApp/app/views.py
@@ -2,11 +2,47 @@ from app import app
 from flask import render_template, request
 import pyorient
 
+class DBManager:
 
-client = pyorient.OrientDB("52.1.220.20",2424)
-session_id = client.connect("root", "music")
-client.db_open("finaldb", "admin", "admin" )
+	def __init__(self):
+		self.serverD2name = "ec2-54-173-195-130.compute-1.amazonaws.com"
+		self.serverD3name = "ec2-54-173-189-239.compute-1.amazonaws.com"
+		self.port = 2424
+		self.runningServer = None
+		self.client = None
+
+	def connectToDBServer(self):
+		
+		targetServer = self.getAvailableServer()
+		
+		try:
+			print ">>> attempting to connect to:         "+targetServer
+			self.runningServer = targetServer
+			self.client = pyorient.OrientDB(targetServer, self.port)
 			
+			session_id = self.client.connect("root", "music")	
+
+		except pyorient.exceptions.PyOrientConnectionException:
+
+			self.runningServer = self.getAvailableServer()
+			print ">>> attempting instead to connect to: "+self.runningServer
+			self.client = pyorient.OrientDB(self.runningServer, self.port)
+			self.session_id = self.client.connect("root", "music")
+
+		print ">>> connected to server:              " + self.runningServer	
+
+	def getAvailableServer(self):
+		availableServer = None
+		if self.runningServer == self.serverD2name:
+			availableServer = self.serverD3name
+		else:
+			availableServer = self.serverD2name
+		return availableServer
+
+
+dbManager = DBManager()
+dbManager.connectToDBServer()
+dbManager.client.db_open("finaldb", "admin", "admin")
 
 @app.route('/')
 @app.route('/index')
@@ -18,7 +54,6 @@ def email_post():
 	depthLevel = 2;	
 	artist_name = request.form["artistName"].title()
 	queryType = "in" if (request.form["queryTypeSelector"] == "Influencers") else "out"
-	print queryType
  	treeInformation = getTree(depthLevel, artist_name.encode('utf-8'), -1, 0, queryType);
 	return render_template("results.html", title = 'Home', artist=artist_name, treeData=treeInformation)
 
@@ -39,8 +74,14 @@ def getTree(depth, artist, parent, artistDate, direction):
 		return nodeInformation
 
 	statement = "select name, date from (select expand("+direction+"('influences')) from artist where name =\'"+artist+"\')"
-	children = client.query(statement)	
 	
+	try:
+		children = dbManager.client.query(statement)	
+	except (pyorient.exceptions.PyOrientConnectionException, pyorient.exceptions.PyOrientDatabaseException):
+		dbManager.connectToDBServer()
+		dbManager.client.db_open("finaldb", "admin", "admin")
+		children = dbManager.client.query(statement)
+
 	for child in children:
 		if child.name != parent:
 			nodeInformation.extend(getTree(depth-1,child.name, artist, child.date,direction))
