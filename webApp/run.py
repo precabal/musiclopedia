4ea3f1ec4018d39924836614f0c9fcb17aeea129
precabal <pablorecabal@gmail.com>
2015-10-15 16:48:53 -0400

    included tornado for webserver

--- webApp/run.py
+++ webApp/run.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+#from yourapplication import app
+
 
 from app import app
-app.run(host='0.0.0.0', debug = True)
+#app.run(host='0.0.0.0', debug = True)
+
+http_server = HTTPServer(WSGIContainer(app))
+http_server.listen(5000)
+IOLoop.instance().start()
